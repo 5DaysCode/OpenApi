5110a9e093b371277bb6e3f948474fd35c5e8809
5DaysCode <5DaysCode@gmail.com>
2023-10-25 10:17:45 +0200

    initialization

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/src/setup-jest.ts"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/dist/"],
+  globals: {
+    "ts-jest": {
+      tsConfig: "<rootDir>/tsconfig.spec.json",
+      diagnostics: false,
+      isolatedModules: true,
+    },
+  },
+};
---
+++ src/setup-jest.ts
@@ -0,0 +1,14 @@
+import 'jest-preset-angular';
+import 'zone.js/dist/zone'; // Included with Angular CLI.
+import 'zone.js/dist/zone-testing';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
+
+// First, initialize the Angular testing environment.
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
